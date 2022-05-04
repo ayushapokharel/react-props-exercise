96c31bcb5b505489a26f05dc836e61563984ceab
Ayusha Pokharel <98035463+ayushapokharel@users.noreply.github.com>
2022-05-03 21:02:07 -0400

    Add files via upload

---
+++ src/Exercise/Exercise.js
@@ -0,0 +1,24 @@
+import Bonus from "../Bonus/Bonus"
+
+function Exercise({good ="Great", bad = "Not Great"}) {
+  return(  <div>
+            <h1>{good}</h1>
+            <h1>{bad}</h1>
+            <Bonus />
+        </div>);
+} 
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    export default Exercise;
